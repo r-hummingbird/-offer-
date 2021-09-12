5c678ccf563f8b421a43277712547805205f4006
sy2021115-任心怡 <849781231@qq.com>
2021-09-13 07:22:21 +0800

    day10

--- src/jianzhioffer/lengthOfLongestSubstring.java
+++ src/jianzhioffer/lengthOfLongestSubstring.java
@@ -1,5 +1,30 @@
 package jianzhioffer;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class lengthOfLongestSubstring {
-    
+    public static int lengthOfLongestSubstring(String s) {
+        int res = 0;
+        Set<Character> set = new HashSet<>();
+        for(int l = 0, r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            while(set.contains(c)) {
+                set.remove(s.charAt(l++));
+            }
+            set.add(c);
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String st;
+        Scanner sc=new Scanner(System.in);
+        st=sc.nextLine();
+        System.out.println(lengthOfLongestSubstring(st));
+    }
+
 }
