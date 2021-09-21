c5990a2c73c09ea6d6f4bc6097e98802bf1ae42f
sy2021115-任心怡 <849781231@qq.com>
2021-09-21 09:18:04 +0800

    day18

--- src/leecode/array/canPlaceFlowers.java
+++ src/leecode/array/canPlaceFlowers.java
@@ -2,22 +2,19 @@ package leecode.array;
 
 public class canPlaceFlowers {
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int num = 0,count = 1;  //假设在数组左边添加0，以解决边界问题，令count初始为1
-        for (int i=0;i<flowerbed.length;i++){
-            if (flowerbed[i] == 0){
-                count++;
-            }else{
-                count = 0;
+        int i = 0;
+        int len = flowerbed.length;
+        while (i < len) {
+            boolean b = flowerbed[i] == 0;
+            boolean b2 = i == 0 || flowerbed[i-1] == 0;
+            boolean b3 = i == len-1 || flowerbed[i+1] == 0;
+            if (b && b2 && b3) {
+                flowerbed[i] = 1;
+                n--;
             }
-            if (count == 3){    //每连续三个0种一次花
-                num++;
-                count = 1;
-            }
+            i++;
         }
-        if (count == 2){    //如果最后count为2而不是1，表示最后一个位置可以种花
-            num++;
-        }
-        return n <= num;
+        return n <= 0;
 
     }
 
