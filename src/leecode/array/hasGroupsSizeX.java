d012e6146665a4d3a32c4ed6838319000f10492a
sy2021115-任心怡 <849781231@qq.com>
2021-09-22 09:37:03 +0800

    day019

---
+++ src/leecode/array/hasGroupsSizeX.java
@@ -0,0 +1,32 @@
+package leecode.array;
+
+
+
+public class hasGroupsSizeX {
+    public static boolean hasGroupsSizeX(int[] deck) {
+        int[] counter = new int[10000];
+        for (int num: deck) {
+            counter[num]++;
+        }
+        int x = 0;
+        for(int cnt: counter) {
+            if (cnt > 0) {
+                x = gcd(x, cnt);
+                if (x == 1) {
+                    return false;
+                }
+            }
+        }
+        return x >1;
+    }
+
+    private static int gcd(int x, int cnt) {
+        return cnt==0?x:gcd(cnt,x%cnt);
+    }
+
+
+    public static void main(String[] args) {
+        int[] deck=new int[]{1,2,3,4,4,3,2,1};
+        System.out.println(hasGroupsSizeX(deck));
+    }
+}
