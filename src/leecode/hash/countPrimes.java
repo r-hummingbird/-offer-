6e4ccf3324d2fb4ece6010cd5131b9fa64b09ed1
sy2021115-任心怡 <849781231@qq.com>
2021-09-24 13:23:06 +0800

    day21

--- src/leecode/hash/countPrimes.java
+++ src/leecode/hash/countPrimes.java
@@ -7,15 +7,16 @@ public class countPrimes {
     public static int countPrimes(int n) {
         int count=0;
         boolean[] flag=new boolean[n];
-        if (n==0||n==1){
-            count=0;
-        }
-        for(int i=2;i<n;i++){
-            if(!flag[i]){
-                count++;
-                for(int j=i+i;j<n;j+=i)
+        for(int i=2;i<=Math.sqrt(n);i++){
+            //如果是质数，就把所有质数的倍数设定为非质数
+            if(!flag[i])
+                for(int j=i*i;j<n;j+=i){
                     flag[j]=true;
-            }
+                }
+        }
+        //遍历计算质数个数
+        for(int i=2;i<n;i++){
+            if(!flag[i]) count++;
         }
         return count;
     }
