957f83c68842d444ad804fc6bddd200469d3f48f
sy2021115-任心怡 <849781231@qq.com>
2021-09-30 12:18:17 +0800

    day27

---
+++ src/leecode/BinarySearch/nextGreatestLetter.java
@@ -0,0 +1,28 @@
+package leecode.BinarySearch;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class nextGreatestLetter {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        
+        int length = letters.length;
+        int low = 0, high = length - 1;
+        // 开始二分查找
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low < length ? letters[low] : letters[0];
+    }
+
+    public static void main(String[] args) {
+        char[] letters=new char[]{'z', 'a', 'b'};
+        char target='c';
+        System.out.println(nextGreatestLetter(letters,target));
+    }
+}
