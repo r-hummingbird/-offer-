1daded1560c5418f321ebbf0c470596c594251dd
sy2021115-任心怡 <849781231@qq.com>
2021-09-30 17:57:32 +0800

    day27

---
+++ src/leecode/BinarySearch/searchRange.java
@@ -0,0 +1,40 @@
+package leecode.BinarySearch;
+
+public class searchRange {
+    public static int[] searchRange(int[] nums, int target) {
+        int[] res = new int[] {-1, -1};
+        res[0] = binarySearch(nums, target, true);
+        res[1] = binarySearch(nums, target, false);
+        return res;
+    }
+
+    private static int binarySearch(int[] nums, int target, boolean leftOrRight) {
+        int res = -1;
+        int left = 0, right = nums.length - 1, mid;
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+            if(target < nums[mid])
+                right = mid - 1;
+            else if(target > nums[mid])
+                left = mid + 1;
+            else {
+                res = mid;
+                //处理target == nums[mid]
+                if(leftOrRight)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums=new int[]{5,7,7,8,8,10};
+        int target=8;
+        int[] res=searchRange(nums,target);
+        for (int item:res){
+            System.out.print(item+" ");
+        }
+    }
+}
