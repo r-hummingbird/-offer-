650d20f4f80eda1ff29ea7077f1f5e7e4ffb68ec
sy2021115-任心怡 <849781231@qq.com>
2021-10-02 11:03:16 +0800

    day29

--- src/jianzhioffer/nthUglyNumber.java
+++ src/jianzhioffer/nthUglyNumber.java
@@ -1,4 +1,27 @@
 package jianzhioffer;
 
 public class nthUglyNumber {
+    public static int nthUglyNumber(int n) {
+        if (n <= 0)
+            return -1;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int id2 = 0, id3 = 0, id5 = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.min(dp[id2] * 2, Math.min(dp[id3] *3, dp[id5] * 5));
+            //三个链表可能有相同元素，所以只要是最小的，都要移动指针
+            if (dp[id2] * 2 == dp[i])
+                id2 += 1;
+            if (dp[id3] * 3 == dp[i])
+                id3 += 1;
+            if (dp[id5] * 5 == dp[i])
+                id5 += 1;
+        }
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(nthUglyNumber(n));
+    }
 }
