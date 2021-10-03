8a73327dd7c6a9e95c352ae66b1de7e5f9e1754d
sy2021115-任心怡 <849781231@qq.com>
2021-10-04 00:12:23 +0800

    day30

---
+++ src/leecode/doublePoint/doublePoint.java
@@ -0,0 +1,34 @@
+package leecode.doublePoint;
+
+import java.awt.event.MouseAdapter;
+
+public class doublePoint {
+    public static int characterReplacement(String s, int k) {
+        int len=s.length();
+        if (len<2)
+            return len;
+        char[] ch=s.toCharArray();
+        int l=0,r=0;
+        int maxCount=0;
+        int[] fre=new int[26];
+        while (r<len){
+            fre[ch[r]-'A']++;
+            maxCount=Math.max(maxCount,fre[ch[r]-'A']);
+
+            if (r-l+1>maxCount+k){
+                //则k不够用
+                fre[ch[l]-'A']--;
+                l++;
+            }
+            r++;
+        }
+        return r-l;
+    }
+
+
+    public static void main(String[] args) {
+        String s="ABAB";
+        int k=2;
+        System.out.println(characterReplacement(s,k));
+    }
+}
